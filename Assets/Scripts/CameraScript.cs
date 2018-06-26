17be57a49855dbfee7f808f2071bc01c8af83383
Schumann <hc_schumann@hotmail.com>
2018-06-26 00:29:50 -0300

    Player mov + camera

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public GameObject player;       //Public variable to store a reference to the player game object
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // LateUpdate is called after Update each frame
+    void LateUpdate()
+    {
+        Vector3 currentPlayerPos = player.transform.position;
+
+        float angleDelta = player.transform.eulerAngles.y - transform.eulerAngles.y;
+
+        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
+        transform.position = currentPlayerPos;
+
+        transform.Translate(Vector3.forward * -34.2f);
+        transform.Translate(Vector3.up * 1);
+    }
+}
---
+++ Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour
+{
+    public float movementSpeed = 10;
+    public float turningSpeed = 60;
+
+    AudioSource playerAudio;
+
+    // Use this for initialization
+    void Start()
+    {
+        playerAudio = GetComponent<AudioSource>();
+    }
+
+    // Updates player color
+    void UpdatePlayerColor()
+    {
+        Renderer renderer = GetComponent<Renderer>();
+        if (Input.GetKey(KeyCode.Space))
+        {
+            renderer.material.color = Color.yellow;
+        }
+        else
+        {
+            renderer.material.color = Color.green;
+        }
+    }
+
+    void HandlePlayerTranslation()
+    {
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 15.0f;
+        transform.Translate(0, 0, z);
+
+        if (Mathf.Abs(z) >= 0.001f)
+        {
+            playerAudio.UnPause();
+        }
+        else
+        {
+            playerAudio.Pause();
+        }
+    }
+    void HandlePlayerRotation()
+    {
+        Vector3 mousePos = Input.mousePosition;
+
+        //To make mousePos relative to center of screen
+        mousePos.x -= Screen.width / 2;
+        mousePos.y -= Screen.height / 2;
+
+        //To make mousePos relative to transform
+        mousePos += transform.position;
+        var angle = Vector3.Angle(mousePos, Vector3.up);
+
+        //For 360 degree angle
+        if (mousePos.x > 0)
+        {
+            angle = 360 - angle;
+        }
+
+        transform.rotation = Quaternion.Euler(0, -angle, 0);
+    }
+    //Handles Player Inputs
+    void HandleInputs()
+    {
+        HandlePlayerRotation();
+
+        HandlePlayerTranslation();
+    }
+
+    void HandleAudio()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdatePlayerColor();
+
+        HandleInputs();
+
+        HandleAudio();
+    }
+}
